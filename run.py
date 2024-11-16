883da919e1cd7c25fadf293a11679c4f7cad4144
Strykeros <edijsz@tutanota.com>
2024-11-16 15:14:49 +0200

    adding sample data

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from scripts.init_db import initialize_db
+from scripts.init_database import initialize_db
 
 if __name__ == '__main__':
     initialize_db()
--- scripts/init_database.py
+++ scripts/init_database.py
@@ -1,6 +1,3 @@
-import sys
-import os
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from models.database import db
 from models.course import Course
 from models.question import Question
